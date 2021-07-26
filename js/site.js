2a6001ccba619993ec8a5e190e481da74e1c660e
Simarpreet <simarpreetsingh448@gmail.com>
2021-07-26 23:22:19 +1200

    working on drop down menu

---
+++ js/site.js
@@ -0,0 +1,26 @@
+
+$(document).ready
+(function()
+{
+    $("#highlight1").mouseover(function()
+    {
+        $("#highlightpera1").css("color","white");
+    })
+    $("#highlight1").mouseleave(function()
+    {
+        $("#highlightpera1").css("color","#888888");
+    }) 
+})
+
+$(document).ready
+(function()
+{
+    $("#highlight2").mouseover(function()
+    {
+        $("#highlightpera2").css("color","white");
+    })
+    $("#highlight2").mouseleave(function()
+    {
+        $("#highlightpera2").css("color","#888888");
+    }) 
+})
